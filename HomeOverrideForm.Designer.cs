5d915bab92b01744270739cec1475ae6a51adfe7
squibonacid <->
2016-06-26 10:34:43 +0200

    1.0

---
+++ HomeOverrideForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace HomeOverride {
+    partial class HomeOverrideForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeOverrideForm));
+            this.tooltip_application = new System.Windows.Forms.ToolTip(this.components);
+            this.tooltip_startwithwindows = new System.Windows.Forms.ToolTip(this.components);
+            this.tooltip_startminimized = new System.Windows.Forms.ToolTip(this.components);
+            this.tooltip_hotkey = new System.Windows.Forms.ToolTip(this.components);
+            this.button_selectYourApplication = new System.Windows.Forms.Button();
+            this.checkbox_enabled = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBox_StartWithWindows = new System.Windows.Forms.CheckBox();
+            this.checkBox_StartMinimized = new System.Windows.Forms.CheckBox();
+            this.button_about = new System.Windows.Forms.Button();
+            this.pictureBox_applicationIcon = new System.Windows.Forms.PictureBox();
+            this.label_AppName = new System.Windows.Forms.TextBox();
+            this.comboBox_KeyMod = new System.Windows.Forms.ComboBox();
+            this.comboBox_Key = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_HotKeyInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_applicationIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_selectYourApplication
+            // 
+            this.button_selectYourApplication.Location = new System.Drawing.Point(72, 36);
+            this.button_selectYourApplication.Name = "button_selectYourApplication";
+            this.button_selectYourApplication.Size = new System.Drawing.Size(45, 23);
+            this.button_selectYourApplication.TabIndex = 41;
+            this.button_selectYourApplication.Text = "Select";
+            this.button_selectYourApplication.UseVisualStyleBackColor = true;
+            this.button_selectYourApplication.Click += new System.EventHandler(this.button_SelectApplication);
+            // 
+            // checkbox_enabled
+            // 
+            this.checkbox_enabled.AutoSize = true;
+            this.checkbox_enabled.Location = new System.Drawing.Point(142, 40);
+            this.checkbox_enabled.Name = "checkbox_enabled";
+            this.checkbox_enabled.Size = new System.Drawing.Size(65, 17);
+            this.checkbox_enabled.TabIndex = 47;
+            this.checkbox_enabled.Text = "Enabled";
+            this.checkbox_enabled.UseVisualStyleBackColor = true;
+            this.checkbox_enabled.CheckedChanged += new System.EventHandler(this.checkbox_enabled_CheckedChanged);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "TestText";
+            this.notifyIcon.BalloonTipTitle = "TestTitle";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Home Override";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // checkBox_StartWithWindows
+            // 
+            this.checkBox_StartWithWindows.AutoSize = true;
+            this.checkBox_StartWithWindows.Location = new System.Drawing.Point(12, 71);
+            this.checkBox_StartWithWindows.Name = "checkBox_StartWithWindows";
+            this.checkBox_StartWithWindows.Size = new System.Drawing.Size(117, 17);
+            this.checkBox_StartWithWindows.TabIndex = 50;
+            this.checkBox_StartWithWindows.Text = "Start with Windows";
+            this.checkBox_StartWithWindows.UseVisualStyleBackColor = true;
+            this.checkBox_StartWithWindows.CheckedChanged += new System.EventHandler(this.checkBox_StartWithWindows_CheckedChanged);
+            // 
+            // checkBox_StartMinimized
+            // 
+            this.checkBox_StartMinimized.AutoSize = true;
+            this.checkBox_StartMinimized.Location = new System.Drawing.Point(12, 94);
+            this.checkBox_StartMinimized.Name = "checkBox_StartMinimized";
+            this.checkBox_StartMinimized.Size = new System.Drawing.Size(96, 17);
+            this.checkBox_StartMinimized.TabIndex = 51;
+            this.checkBox_StartMinimized.Text = "Start minimized";
+            this.checkBox_StartMinimized.UseVisualStyleBackColor = true;
+            this.checkBox_StartMinimized.CheckedChanged += new System.EventHandler(this.checkBox_StartMinimized_CheckedChanged);
+            // 
+            // button_about
+            // 
+            this.button_about.Location = new System.Drawing.Point(116, 36);
+            this.button_about.Name = "button_about";
+            this.button_about.Size = new System.Drawing.Size(23, 23);
+            this.button_about.TabIndex = 52;
+            this.button_about.Text = "?";
+            this.button_about.UseVisualStyleBackColor = true;
+            this.button_about.Click += new System.EventHandler(this.button_about_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox_applicationIcon.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_applicationIcon.Name = "pictureBox1";
+            this.pictureBox_applicationIcon.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_applicationIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_applicationIcon.TabIndex = 54;
+            this.pictureBox_applicationIcon.TabStop = false;
+            // 
+            // label_AppName
+            // 
+            this.label_AppName.Location = new System.Drawing.Point(72, 14);
+            this.label_AppName.Name = "label_AppName";
+            this.label_AppName.Size = new System.Drawing.Size(135, 20);
+            this.label_AppName.TabIndex = 48;
+            // 
+            // comboBox_KeyMod
+            // 
+            this.comboBox_KeyMod.FormattingEnabled = true;
+            this.comboBox_KeyMod.Location = new System.Drawing.Point(12, 130);
+            this.comboBox_KeyMod.Name = "comboBox_KeyMod";
+            this.comboBox_KeyMod.Size = new System.Drawing.Size(68, 21);
+            this.comboBox_KeyMod.TabIndex = 55;
+            this.comboBox_KeyMod.SelectedIndexChanged += new System.EventHandler(this.comboBox_KeyMod_SelectedIndexChanged);
+            // 
+            // comboBox_Key
+            // 
+            this.comboBox_Key.FormattingEnabled = true;
+            this.comboBox_Key.Location = new System.Drawing.Point(105, 130);
+            this.comboBox_Key.Name = "comboBox_Key";
+            this.comboBox_Key.Size = new System.Drawing.Size(102, 21);
+            this.comboBox_Key.TabIndex = 56;
+            this.comboBox_Key.SelectedIndexChanged += new System.EventHandler(this.comboBox_Key_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Hotkey:";
+            // 
+            // label_HotKeyInfo
+            // 
+            this.label_HotKeyInfo.BackColor = System.Drawing.Color.White;
+            this.label_HotKeyInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label_HotKeyInfo.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label_HotKeyInfo.Enabled = false;
+            this.label_HotKeyInfo.Location = new System.Drawing.Point(136, 71);
+            this.label_HotKeyInfo.Multiline = true;
+            this.label_HotKeyInfo.Name = "label_HotKeyInfo";
+            this.label_HotKeyInfo.ReadOnly = true;
+            this.label_HotKeyInfo.Size = new System.Drawing.Size(71, 56);
+            this.label_HotKeyInfo.TabIndex = 59;
+            // 
+            // StartupSeviceForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(219, 161);
+            this.Controls.Add(this.label_HotKeyInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_Key);
+            this.Controls.Add(this.comboBox_KeyMod);
+            this.Controls.Add(this.pictureBox_applicationIcon);
+            this.Controls.Add(this.button_about);
+            this.Controls.Add(this.checkBox_StartMinimized);
+            this.Controls.Add(this.checkBox_StartWithWindows);
+            this.Controls.Add(this.label_AppName);
+            this.Controls.Add(this.checkbox_enabled);
+            this.Controls.Add(this.button_selectYourApplication);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(235, 200);
+            this.MinimumSize = new System.Drawing.Size(235, 200);
+            this.Name = "StartupSeviceForm";
+            this.Text = "Home Override";
+            this.Resize += new System.EventHandler(this.StartupSeviceForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_applicationIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip tooltip_application;
+        private System.Windows.Forms.ToolTip tooltip_startwithwindows;
+        private System.Windows.Forms.ToolTip tooltip_startminimized;
+        private System.Windows.Forms.ToolTip tooltip_hotkey;
+        private System.Windows.Forms.Button button_selectYourApplication;
+        private System.Windows.Forms.CheckBox checkbox_enabled;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.CheckBox checkBox_StartWithWindows;
+        private System.Windows.Forms.CheckBox checkBox_StartMinimized;
+        private System.Windows.Forms.Button button_about;
+        private System.Windows.Forms.PictureBox pictureBox_applicationIcon;
+        private System.Windows.Forms.TextBox label_AppName;
+        private System.Windows.Forms.ComboBox comboBox_KeyMod;
+        private System.Windows.Forms.ComboBox comboBox_Key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox label_HotKeyInfo;
+    }
+}
+
---
+++ HomeOverrideForm.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+namespace HomeOverride {
+    public partial class HomeOverrideForm : Form {
+        HomeOverrideProgram homeOverrideProgram;
+        ContextMenu contextMenu;
+
+        public HomeOverrideForm() {
+            string[] args = Environment.GetCommandLineArgs();
+            InitializeComponent();
+            homeOverrideProgram = new HomeOverrideProgram(this);
+            contextMenu = new ContextMenu();
+            MenuItem m1 = contextMenu.MenuItems.Add("Open");
+            m1.Click += new System.EventHandler(this.notifyIcon_DoubleClick);
+            notifyIcon.ContextMenu = contextMenu;
+
+            tooltip_application.SetToolTip(label_AppName, "Select application that you want to launch when Oculus Home is starting or the hotkey is pressed");
+            tooltip_application.SetToolTip(button_selectYourApplication, "Select the application that launches when Oculus Home is starting or the hotkey is pressed");
+            tooltip_application.SetToolTip(button_about, "Info");
+            tooltip_application.SetToolTip(checkbox_enabled, "enable/disable this tool");
+            tooltip_hotkey.SetToolTip(comboBox_KeyMod, "What hotkey do you want to use to launch your application?");
+            tooltip_hotkey.SetToolTip(comboBox_Key, "What hotkey do you want to use to launch your application?");
+            tooltip_startminimized.SetToolTip(checkBox_StartMinimized, "Should this tool start minimized (to tray)?");
+            tooltip_startwithwindows.SetToolTip(checkBox_StartWithWindows, "Should this tool start up with windows?");
+        }
+
+        public class GlobalHotKey {
+            public const int WM_HOTKEY_MSG_ID = 0x0312;
+            public enum Modifier {
+                None = 0x0000,
+                ALT = 0x0001,
+                CTRL = 0x0002,
+                SHIFT = 0x0004,
+                WIN = 0x0008
+            }
+
+            [DllImport("user32.dll")]
+            private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+            [DllImport("user32.dll")]
+            private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+            private int modifier;
+            private int key;
+            private IntPtr hWnd;
+            private int id;
+
+            public GlobalHotKey(int modifier, Keys key, Form form) {
+                this.modifier = modifier;
+                this.key = (int)key;
+                this.hWnd = form.Handle;
+                id = this.GetHashCode();
+            }
+
+            public override int GetHashCode() {
+                return modifier ^ key ^ hWnd.ToInt32();
+            }
+
+            public bool Register() {
+                return RegisterHotKey(hWnd, id, modifier, key);
+            }
+
+            public bool Unregister() {
+                return UnregisterHotKey(hWnd, id);
+            }
+        }
+
+        public class HomeOverrideProgram {
+
+            static GlobalHotKey hotkey;
+            static HomeOverrideForm form;
+            static System.Threading.Timer timer;
+
+            bool wasOculusHomeRunningLastUpdate = false;
+            int myProcessID = -1;
+            Process myProcess = null;
+            
+            public HomeOverrideProgram(HomeOverrideForm _form) {
+                form = _form;
+                Configuration.Load();
+                
+                InitializeForm();
+                InitializeTimer();
+                InitializeHotkey();
+                RegisterHotkey();
+                RegisterInStartup(form.checkBox_StartWithWindows.Checked);
+            }
+
+            public void RegisterHotkey() {
+                if (hotkey != null) {
+                    hotkey.Unregister();
+                }
+
+                Configuration.selectedHotKeyKey = form.comboBox_Key.SelectedIndex;
+                Configuration.selectedHotKeyMod = form.comboBox_KeyMod.SelectedIndex;
+                Configuration.Save();
+
+                GlobalHotKey.Modifier mod = (GlobalHotKey.Modifier)form.comboBox_KeyMod.SelectedItem;
+                Keys key = (Keys)form.comboBox_Key.SelectedItem;
+                hotkey = new GlobalHotKey((int)mod, key, form);
+                bool success = hotkey.Register();
+
+                form.label_HotKeyInfo.Text = success ? "" : "Could not register Hotkey " + mod + "+" + key;
+                form.label_HotKeyInfo.ForeColor = success ? System.Drawing.Color.Black : System.Drawing.Color.DarkRed;
+            }
+            
+            public void RegisterInStartup(bool register) {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+                        ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                if (register) {
+                    registryKey.SetValue("Oculus Home Override", Application.ExecutablePath);
+                } else {
+                    if (registryKey.GetValue("Oculus Home Override") != null) {
+                        registryKey.DeleteValue("Oculus Home Override");
+                    }
+                }
+            }
+
+            public void SelectApplication() {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                openFileDialog.Filter = ".exe files (*.exe)|*.exe";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    string result = openFileDialog.FileName;
+                    Configuration.customApplicationPath = result;
+                    Configuration.Save();
+                    form.label_AppName.Text = Configuration.customApplicationPath;
+                }
+
+                if (File.Exists(Configuration.customApplicationPath)) {
+                    form.pictureBox_applicationIcon.Image = System.Drawing.Icon.ExtractAssociatedIcon(Configuration.customApplicationPath).ToBitmap();
+                }
+            }
+            
+            public void HotKeyPressed() {
+                StartOrRestartCustomApplication();
+            }
+            
+            public bool HasAutostartEntry() {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+                        ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                return registryKey.GetValue("Oculus Home Override") != null;
+            }
+
+            void OnTimerUpdate() {
+                bool isOculusHomeRunning = ProcessExists("OculusVR");
+
+                if (isOculusHomeRunning) {
+                    if (myProcess == null || myProcessID == -1 || !wasOculusHomeRunningLastUpdate) {
+                        StartOrRestartCustomApplication();
+                    }
+                }
+
+                wasOculusHomeRunningLastUpdate = isOculusHomeRunning;
+            }
+
+            void InitializeForm() {
+                form.checkBox_StartMinimized.Checked = Configuration.startMinimized;
+                form.checkbox_enabled.Checked = Configuration.enabled;
+                form.checkBox_StartWithWindows.Checked = HasAutostartEntry();
+                form.label_AppName.Text = Path.GetFileName(Configuration.customApplicationPath);
+                if (File.Exists(Configuration.customApplicationPath)) {
+                    form.pictureBox_applicationIcon.Image = System.Drawing.Icon.ExtractAssociatedIcon(Configuration.customApplicationPath).ToBitmap();
+                }
+                if (Configuration.startMinimized) {
+                    form.MinimizeToTray();
+                }
+            }
+
+            void InitializeTimer() {
+                timer = new System.Threading.Timer(
+                    e => OnTimerUpdate(),
+                    null,
+                    TimeSpan.Zero,
+                    TimeSpan.FromSeconds(1f));
+            }
+
+            void InitializeHotkey() {
+                form.comboBox_KeyMod.Items.Clear();
+                form.comboBox_KeyMod.Items.AddRange(Enum.GetValues(typeof(GlobalHotKey.Modifier)).Cast<object>().ToArray());
+                form.comboBox_KeyMod.SelectedIndex = Configuration.selectedHotKeyMod;
+                
+                form.comboBox_Key.Items.Clear();
+                form.comboBox_Key.Items.AddRange(Enum.GetValues(typeof(Keys)).Cast<object>().ToArray());
+                form.comboBox_Key.SelectedIndex = Configuration.selectedHotKeyKey;
+            }
+
+            void StartOrRestartCustomApplication() {
+                if (!Configuration.enabled || !File.Exists(Configuration.customApplicationPath)) {
+                    return;
+                }
+
+                if (myProcess != null) {
+                    while (!myProcess.HasExited) {
+                        try {
+                            myProcess.Kill();
+                        } catch { }
+                    }
+                } while (ProcessExists(myProcessID)) {
+                    try {
+                        Process process = Process.GetProcessById(myProcessID);
+                        process.Kill();
+                    } catch { }
+                }
+                
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = Configuration.customApplicationPath;
+                startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                Process newProcess = Process.Start(startInfo);
+                myProcessID = newProcess.Id;
+                myProcess = newProcess;
+            }
+
+            bool ProcessExists(int id) {
+                return Process.GetProcesses().Any(x => x.Id == id);
+            }
+
+            bool ProcessExists(string name) {
+                return Process.GetProcesses().Any(x => x.ProcessName == name);
+            }
+
+        }
+
+        public static class Configuration {
+
+            public static string customApplicationPath = "";
+            public static bool enabled = true;
+            public static bool startMinimized = false;
+            public static int selectedHotKeyKey = 0;
+            public static int selectedHotKeyMod = 0;
+
+            public enum ConfigParam { CustomApplicationPath = 0, Enabled = 1, StartMinimized = 2, HotKeyKey = 3, HotKeyMod = 4 }
+
+            public static void Load() {
+                string configPath = (Path.Combine(Application.UserAppDataPath, "config.txt"));
+                if (!File.Exists(configPath)) {
+                    File.Create(configPath);
+                }
+                string[] lines = File.ReadAllLines(configPath);
+                while (lines.Length < 5) {
+                    List<string> temp = lines.ToList();
+                    temp.Add("");
+                    lines = temp.ToArray();
+                }
+                customApplicationPath = lines[(int)ConfigParam.CustomApplicationPath];
+                enabled = lines[(int)ConfigParam.Enabled] == true.ToString();
+                startMinimized = lines[(int)ConfigParam.StartMinimized] == true.ToString();
+
+                selectedHotKeyKey = 0;
+                int.TryParse(lines[(int)ConfigParam.HotKeyKey], out selectedHotKeyKey);
+
+                selectedHotKeyMod = 0;
+                int.TryParse(lines[(int)ConfigParam.HotKeyMod], out selectedHotKeyMod);
+            }
+
+            public static void Save() {
+                string configPath = (Path.Combine(Application.UserAppDataPath, "config.txt"));
+                if (!File.Exists(configPath)) {
+                    File.Create(configPath);
+                }
+
+                File.WriteAllLines(configPath, new string[] {
+                    customApplicationPath,
+                    enabled.ToString(),
+                    startMinimized.ToString(),
+                    selectedHotKeyKey.ToString(),
+                    selectedHotKeyMod.ToString()
+                });
+            }
+
+        }
+
+        #region events
+        protected override void WndProc(ref Message m) {
+            if (m.Msg == GlobalHotKey.WM_HOTKEY_MSG_ID) {
+                homeOverrideProgram.HotKeyPressed();
+            }
+            base.WndProc(ref m);
+        }
+
+        public void MinimizeToTray() {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void StartupSeviceForm_Resize(object sender, System.EventArgs e) {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void notifyIcon_DoubleClick(object sender, MouseEventArgs e) {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon_DoubleClick(object sender, System.EventArgs e) {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void button_SelectApplication(object sender, EventArgs e) {
+            homeOverrideProgram.SelectApplication();
+        }
+
+        private void checkBox_StartWithWindows_CheckedChanged(object sender, EventArgs e) {
+            if (homeOverrideProgram == null) { return; }
+            homeOverrideProgram.RegisterInStartup(checkBox_StartWithWindows.Checked);
+            checkBox_StartWithWindows.Checked = homeOverrideProgram.HasAutostartEntry();
+        }
+
+        private void checkBox_StartMinimized_CheckedChanged(object sender, EventArgs e) {
+            if (homeOverrideProgram == null) { return; }
+            Configuration.startMinimized = checkBox_StartMinimized.Checked;
+            Configuration.Save();
+        }
+
+        private void checkbox_enabled_CheckedChanged(object sender, EventArgs e) {
+            if (homeOverrideProgram == null) { return; }
+            Configuration.enabled = checkbox_enabled.Checked;
+            Configuration.Save();
+        }
+        
+        private void button_about_Click(object sender, EventArgs e) {
+            MessageBox.Show(
+                "Oculus Home Override 1.0\nu/squibonacid\n\n" +
+
+                "Functionality:\n" +
+                "- Switch to your favourite VR application via hotkey\n" +
+                "- Optional: Switch to your favourite VR application\nwhen Oculus Home starts up first time",
+                "Info", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void comboBox_KeyMod_SelectedIndexChanged(object sender, EventArgs e) {
+            if (homeOverrideProgram != null) {
+                homeOverrideProgram.RegisterHotkey();
+            }
+        }
+
+        private void comboBox_Key_SelectedIndexChanged(object sender, EventArgs e) {
+            if (homeOverrideProgram != null) {
+                homeOverrideProgram.RegisterHotkey();
+            }
+        }
+        #endregion events
+    }
+}
